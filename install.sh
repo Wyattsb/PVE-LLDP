64f8b2823dd2be02fddb4a8edfa0add2f59ab64d
Wyattsb <wsbrookshier@gmail.com>
2026-09-13 13:59:42 -0400

    Update install.sh
    Update lldp configuration to surround the interface search in single quotations, and the crontab entry be Idempotent.

--- install.sh
+++ install.sh
@@ -28,27 +28,32 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 echo "Updating package lists..."
-sudo apt update
+apt update
 
 echo "Installing lldpd..."
-sudo apt install lldpd -y
+apt install lldpd -y
 
 echo "Configuring lldpd to enable additional protocols..."
-echo 'DAEMON_ARGS="-x -c -s -e"' | sudo tee /etc/default/lldpd
+echo 'DAEMON_ARGS="-x -c -s -e"' | tee /etc/default/lldpd
 
 echo "Restarting lldpd service..."
-sudo systemctl restart lldpd
+systemctl restart lldpd
 
 echo "Configuring lldpd to monitor all interfaces matching '*' pattern..."
-sudo lldpcli configure system interface pattern *
+lldpcli configure system interface pattern '*'
 
 # Assuming the update_interface_desc.sh is in the same directory as install.sh
 # Copy the update_interface_desc.sh script to /usr/local/bin
 echo "Copying the update_interface_desc.sh script to /usr/local/bin..."
-sudo cp update_interface_desc.sh /usr/local/bin/update_interface_desc.sh
-sudo chmod +x /usr/local/bin/update_interface_desc.sh
+cp update_interface_desc.sh /usr/local/bin/update_interface_desc.sh
+chmod +x /usr/local/bin/update_interface_desc.sh
 
+# Search cron for a matching entry, and replace it with the new/correct entry
+CRON_SEARCH="update_interface_desc.sh"
+NEW_CRON="0 * * * * /usr/local/bin/update_interface_desc.sh >> /var/log/update_interface_desc.log 2>&1"
+
+# The update pipeline
 echo "Creating cron job for updating interface descriptions..."
-(crontab -l 2>/dev/null; echo "0 * * * * /usr/local/bin/update_interface_desc.sh >> /var/log/update_interface_desc.log 2>&1") | crontab -
+(crontab -l 2>/dev/null | grep -v -F "$CRON_SEARCH"; echo "$NEW_CRON") | crontab -
 
 echo "Installation and configuration completed successfully."
